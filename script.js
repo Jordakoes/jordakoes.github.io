da407f71e8d16b15c5bfdc69f14ca0b7c93053eb
Jordakoes <hovenjordi@gmail.com>
2018-10-11 13:14:45 +0200

    Rework entire website

---
+++ script.js
@@ -0,0 +1,14 @@
+ var app = document.getElementById('purple');
+ var typewriter = new Typewriter(purple, {
+     loop: false
+ });
+
+ typewriter.typeString('Desinger')
+ .pauseFor(1500)
+ .deleteAll()
+ .typeString('Developer')
+ .pauseFor(1500)
+ .deleteAll()
+ .typeString('Prototyper')
+ .pauseFor(1500)
+ .start()
